f70c27edfe70770cdaa964c207604b35fbdebb74
kipo495 <hidekipo49@gmail.com>
2025-10-16 21:36:06 +0900

    see

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,34 @@
-import { headers } from 'next/headers';
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink
+} from "@radix-ui/react-navigation-menu";
 
 
 export default function Home() {
   return (
-    <div>
-      <header>
-
+    <div className="p-3">
+      <header className="text-xl font-bold">
+        <NavigationMenu>
+          <NavigationMenuList className="flex space-x-5">
+            <NavigationMenuItem>
+              <NavigationMenuLink href="/" className="hover:underline">
+                Home
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink href="/about" className="hover:underline">
+                About
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink href="/blog" className="hover:underline">
+                Blog
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </header>
       <main>
 
