1bc721f4ae76d55f823ceb6bbb541c9cbb852f74
kipo495 <hidekipo49@gmail.com>
2025-10-16 21:18:27 +0900

    see how localhost acts

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,18 @@
+import { headers } from 'next/headers';
 
 
 export default function Home() {
   return (
+    <div>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
   );
 }
